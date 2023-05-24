01dbf14cbeb0db98e18805246fab73f9a26cf9eb
yeganehNobakht <m.yeganeh.n1994@gmail.com>
2023-05-24 13:44:27 +0330

    correct packaging

--- src/main/java/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/sfg/beer/order/service/config/JmsConfig.java
@@ -1,4 +1,4 @@
-package com.bmc.msscbeerservice.config;
+package sfg.beer.order.service.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
