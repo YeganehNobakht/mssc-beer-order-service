5807b9af539b05740b3095b7a5e5100ed00f5bae
yeganehNobakht <m.yeganeh.n1994@gmail.com>
2023-05-30 12:06:21 +0330

    refactoring model to common package

---
+++ src/main/java/come/bmc/sfg/beer/order/service/domain/BeerOrderStatusEnum.java
@@ -0,0 +1,6 @@
+package come.bmc.sfg.beer.order.service.domain;
+
+public enum BeerOrderStatusEnum {
+    NEW, VALIDATED, VALIDATION_EXCEPTION, ALLOCATED, ALLOCATION_EXCEPTION,
+    PENDING_INVENTORY, PICKED_UP, DELIVERED, DELIVERY_EXCEPTION
+}
