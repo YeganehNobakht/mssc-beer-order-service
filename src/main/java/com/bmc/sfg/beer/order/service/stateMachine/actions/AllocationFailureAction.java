0815a0a24a69159954cd71dcdccb17a6deb96796
yeganehNobakht <m.yeganeh.n1994@gmail.com>
2023-06-12 10:51:12 +0330

    modifing order status and order events

---
+++ src/main/java/com/bmc/sfg/beer/order/service/stateMachine/actions/AllocationFailureAction.java
@@ -0,0 +1,5 @@
+package com.bmc.sfg.beer.order.service.stateMachine.actions;/**
+ * @author Masoumeh Yeganeh
+ * @created 12/06/2023
+ */public class AllocationFailureAction {
+}
---
+++ src/main/java/com/bmc/sfg/brewery/model/events/AllocationFailureEvent.java
@@ -0,0 +1,5 @@
+package com.bmc.sfg.brewery.model.events;/**
+ * @author Masoumeh Yeganeh
+ * @created 12/06/2023
+ */public class AllocationFailureEvent {
+}
